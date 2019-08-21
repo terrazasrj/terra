ab62b265769a33ed41a28a5ce1680f40d421247c
terrazasrj <54365733+terrazasrj@users.noreply.github.com>
2019-08-21 07:35:56 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/*****************************************************************************
+* Hello.java
+*****************************************************************************
+*First Lab Project
+*____________________________________________________________________________
+*Romel Terrazas
+*8/21/2019
+* Comp sci 255 Section 1
+****************************************************************************/
+
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Romel Terrazas");
+		System.out.println(" Hello.java");
+		System.out.println("First Lab Project");
+		System.out.println("8/21/2019");
+		System.out.println();
+	}
+	
+	public static void main (String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+} 	
